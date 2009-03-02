556a2fa886ec9a925f1a6fd5e260aaddb1dcbff6
Reinhard Tartler <siretart@tauware.de>
2009-03-02 23:11:16 +0100

    add a get-orig-source.sh script
    this script is heavily based on the get-orig-source.sh script found in
    the ffmpeg and the aspectc++ package. call it in the top level source
    directory like this:
    
    sh -x debian/get-orig-source.sh -d 20090301

---
+++ debian/get-orig-source.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+#
+#  Script to create a 'pristine' tarball for the debian mplayer source package
+#  Copyright (C) 2009, Reinhard Tartler
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+set -eu
+
+usage() {
+	cat >&2 <<EOF
+usage: $0 [-dh]
+  -h : display help
+  -r : svn revision
+  -o : output tarball name
+  -c : path to cleanup script
+EOF
+}
+
+debug () {
+	$DEBUG && echo "DEBUG: $*" >&2
+}
+
+error () {
+	echo "$1" >&2
+	exit 1;
+}
+
+set +e
+PARAMS=`getopt hd: "$@"`
+if test $? -ne 0; then usage; exit 1; fi;
+set -e
+
+eval set -- "$PARAMS"
+
+DEBUG=false
+SVNDATE=
+
+while test $# -gt 0
+do
+	case $1 in
+		-h) usage; exit 1 ;;
+		-d) SVNDATE=$2; shift ;;
+		--) shift ; break ;;
+		*)  echo "Internal error!" ; exit 1 ;;
+	esac
+	shift
+done
+
+# sanity checks now
+dh_testdir
+
+if [ -z $SVNDATE ]; then
+	error "you need to specify an svn date. e.g. 20081230 for Dec 29. 2008"
+fi
+
+CLEANUPSCRIPT=`pwd`/debian/strip.sh
+TARBALL=../mplayer_0.svn${SVNDATE}.orig.tar.gz
+TARBALL_UNSTRIPPED=../mplayer-non-DFSG_0.svn${SVNDATE}.orig.tar.gz
+PACKAGENAME=mplayer
+
+TMPDIR=`mktemp -d`
+trap 'rm -rf ${TMPDIR}'  EXIT
+
+baseurl="svn://svn.mplayerhq.hu/mplayer/trunk"
+
+svn export -r{${SVNDATE}} \
+	--ignore-externals \
+	${baseurl}  \
+	${TMPDIR}/${PACKAGENAME}
+
+svn info -r{${SVNDATE}} \
+	${baseurl} \
+	| awk '/^Revision/ {print $2}' \
+	> ${TMPDIR}/${PACKAGENAME}/.svnrevision
+
+# get svn externals
+svn pg svn:externals $baseurl | \
+while read external url; do
+    [ -z $url ] && continue
+    dest="${TMPDIR}/${PACKAGENAME}/${external}"
+    svn export -r{${SVNDATE}} --ignore-externals $url $dest
+    svn info $url -r{${SVNDATE}} \
+      | awk '/^Revision/ {print $2}' \
+      > ${TMPDIR}/${PACKAGENAME}/${external}/.svnrevision
+done
+
+tar czf ${TARBALL_UNSTRIPPED} -C ${TMPDIR} ${PACKAGENAME}
+	
+( cd ${TMPDIR}/${PACKAGENAME} && sh ${CLEANUPSCRIPT} )
+
+tar czf ${TARBALL} -C ${TMPDIR} ${PACKAGENAME}
+
---
+++ debian/strip.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This contains the dreaded DVD decryption code. We can live without it
+#  by using libdvdread3 (and the optional library installed by
+#    http://www.debian-unofficial.org/   :-)
+rm -rfv libdvdcss
+
+# Well this may seem a bit excessive... But this code is not useful
+#  for building the package, and most of it does not correctly state
+#  author-license-copyright:  So I throw out the baby and the bath...
+# When and if someone needs this stuff, I will carefully scrutinize
+#  it and add what is suitable.
+rm -rf TOOLS
+
+#Check if upstream includes DOCS and then don't rebuild them.
+if [ -r DOCS/HTML ]; then touch DOCS/.upstream_ships_docs; fi
+
+# My debian dir is too different.
+mv debian debian_upstream
+
+# Do not support encoding in any way.
+rm -fv mencoder.c
+
