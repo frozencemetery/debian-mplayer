e2f5171817ca20a7d01dd56599bcea0ecc1fee7c
Reinhard Tartler <siretart@tauware.de>
2009-04-05 10:52:38 +0200

    no longer remove the TOOLS subdirectory
    This is done on upstream's request (Diego Biurrun). Since it was
    implemented a lot of cleanups have been done upstream. If someone finds
    questionable content in here, please report it as a bug, we'll discuss
    this then with Diego.
    
    debian/copyright probably needs updating, though.

--- debian/strip.sh
+++ debian/strip.sh
@@ -5,13 +5,6 @@
 #    http://www.debian-unofficial.org/   :-)
 rm -rfv libdvdcss
 
-# Well this may seem a bit excessive... But this code is not useful
-#  for building the package, and most of it does not correctly state
-#  author-license-copyright:  So I throw out the baby and the bath...
-# When and if someone needs this stuff, I will carefully scrutinize
-#  it and add what is suitable.
-rm -rf TOOLS
-
 # Do not support encoding in any way.
 rm -fv mencoder.c
 
