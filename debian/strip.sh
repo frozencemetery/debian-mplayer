683c6747c6150d143f3a695c82a4b01e97f55ae7
Reinhard Tartler <siretart@tauware.de>
2009-06-04 09:59:02 +0200

    strip libavcodec similar to how its done in the ffmpeg package
    this ensures that we do not ship mpeg encoders in this package.

--- debian/strip.sh
+++ debian/strip.sh
@@ -4,3 +4,13 @@
 #  by using libdvdread3 (and the optional library installed by
 #    http://www.debian-unofficial.org/   :-)
 rm -rfv libdvdcss
+
+# strip/clean the code from potentially dangerous patented code
+# KEEP THIS IN SYNC WITH THE ffmpeg-debian PACKAGE!
+for codec in 'h263.*' 'h264.*' mpeg2video mpeg4 'msmpeg4.*'; do
+    F=libavcodec/allcodecs.c
+    sed -i "/REGISTER_ENCODER.*\\<$codec\\>/d" $F
+    sed -i "s/REGISTER_ENCDEC\\(.*\\<$codec\\>\\)/REGISTER_DECODER\\1/" $F
+    F=libavcodec/*.c
+    sed -i "/AVCodec *${codec}_encoder *=/,/^[[:space:]]*}/d" $F
+done
