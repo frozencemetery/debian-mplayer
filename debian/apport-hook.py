d1b15f054dd6fa17b2e6fb5faeff1c29e532ea06
Reinhard Tartler <siretart@tauware.de>
2009-10-08 13:09:15 +0200

    install apport hook to include configuration files

---
+++ debian/apport-hook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+'''apport package hook for mplayer
+
+(c) 2009 Author: Reinhard Tartler <siretart@tauware.de>
+'''
+
+from apport.hookutils import *
+from os import path
+
+def add_info(report):
+    # Build System Environment
+    report['system'] = "distro = Ubuntu, architecture = %s, kernel = %s" % (command_output(['uname','-m']), command_output(['uname','-r']))
+
+    attach_related_packages(report, [
+            "libavcodec52",
+            "libavcodec-extra-52",
+            ])
+            
+    attach_file_if_exists(report, path.expanduser('~/.mplayer/config'), 'UserConf')
+    attach_file(report, '/etc/mplayer/mplayer.conf', 'SystemConf')
+
+## DEBUGING ##
+if __name__ == '__main__':
+    report = {}
+    add_info(report)
+    for key in report:
+        print '[%s]\n%s' % (key, report[key])
